0f584b0a7585eb93908201510d74d7d130678d08
Anish Kanna <sautronik@gmail.com>
2022-06-16 14:26:20 +0530

    fixed image loading

--- components/member.tsx
+++ components/member.tsx
@@ -1,29 +1,27 @@
-import React,{FC} from "react";
+import React, { FC } from "react";
 import Image from "next/image";
 
-
-
 interface Props {
-  id:string 
-  name:string
-  socialID: string
+  id: string;
+  name: string;
+  socialID: string;
 }
 
-
-const Member : FC<Props> = ({id,name,socialID}) => {
-  return(
+const Member: FC<Props> = ({ id, name, socialID }) => {
+  return (
     <div>
       <Image
+        blurDataURL={`/assets/member/peep-${id}.svg`}
+        placeholder="blur"
         src={`/assets/member/peep-${id}.svg`}
         alt={name}
-        width={1366} 
+        width={1366}
         height={1555}
-        /> 
+      />
       <div className="text-2xl xl:text-3xl">{name}</div>
       <div className="text-xs">{socialID}</div>
     </div>
-  )
-}
+  );
+};
 
-
-export default Member
+export default Member;
